7f1d26e01c27245b7852c3df427ff70a27e26039
manprreet <129953318+manprreet@users.noreply.github.com>
2024-04-29 17:39:22 +0530

    Add files via upload

---
+++ wp-includes/scripts/PreBuildWordPress.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Perform any pre-build tasks here, such as installing dependencies or setting up environment variables
+cd /var/www/html/jalalpur-cicd
+# Example: Install PHP dependencies (if any)
+# composer install
